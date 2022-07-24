6acba6aa202809dbac67066ce1bdc95d02fbb209
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-24 15:55:34 -0300

    criado classe/created class

---
+++ index.js
@@ -0,0 +1,22 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Charles";
+cliente1.cpf = 11122233345;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+cliente2.nome = "Ricardo";
+cliente2.cpf = 88822233345;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente1);
+console.log(cliente2);
