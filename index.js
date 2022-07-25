0e4fd42abddcfe0fb53a1877a58a4965f260aec2
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-24 22:06:22 -0300

    adicionado regra de negócio sacar à classe/added withdrawal business rule to class

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+    sacar(valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }    
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Charles";
 cliente1.cpf = 11122233345;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Ricardo";
 cliente2.cpf = 88822233345;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrenteCharles = new ContaCorrente();
+contaCorrenteCharles.saldo = 0;
+contaCorrenteCharles.agencia = 1001;
+console.log(contaCorrenteCharles.saldo);
+contaCorrenteCharles.saldo = 100;
+console.log(contaCorrenteCharles.saldo);
+contaCorrenteCharles.sacar(50);
+console.log(contaCorrenteCharles.saldo);
 
 console.log(cliente1);
 console.log(cliente2);
